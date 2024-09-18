5ade7a65274258c10759f6e6c0a2c7633e1df8cf
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-09-18 18:00:30 +0600

    service page running

---
+++ src/components/ui/servicePage/ServiceCardDetails.tsx
@@ -0,0 +1,47 @@
+import { useNavigate } from "react-router-dom";
+import { TService } from "../../../types/service.types";
+import { GiDuration } from "react-icons/gi";
+import { TbCoinTakaFilled } from "react-icons/tb";
+import { FaLongArrowAltRight } from "react-icons/fa";
+
+const ServiceCardDetails = ({ service }: { service: TService }) => {
+    const navigate = useNavigate();
+    return (
+        <div className="bg-white p-4 border border-slate-200">
+            <div className="w-full h-60 sm:h-40 overflow-hidden">
+                <img
+                    className="w-full h-full object-cover"
+                    src={service?.image}
+                    alt={service?.name}
+                />
+            </div>
+            <div className="space-y-2 pt-5">
+                <div className="flex items-center gap-3 text-slate-700">
+                    {/* <div className="flex items-center gap-1 text-sm font-medium">
+                        <FaStar className="text-cws-yellow" />
+                        <span>{service?.rating}</span>
+                    </div> */}
+
+                    <div className="flex items-center gap-1 text-sm font-medium">
+                        <GiDuration className="mt-[1.5px] text-lg" />
+                        <span>{service?.duration} Min</span>
+                    </div>
+                    <div className="flex items-center gap-1 text-sm font-medium">
+                        <TbCoinTakaFilled className="mt-[1.5px] text-lg" />
+                        <span>{service?.price}</span>
+                    </div>
+                </div>
+                <h4 className="text-lg font-semibold">{service?.name}</h4>
+                <p className="text-slate-700">{service?.description}</p>
+                <button
+                    onClick={() => navigate(`/services/${service?._id}`)}
+                    className="text-cws-yellow flex items-center gap-2"
+                >
+                    Book Service <FaLongArrowAltRight className="mt-1" />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default ServiceCardDetails;
--- src/pages/ServicePage.tsx
+++ src/pages/ServicePage.tsx
@@ -1,13 +1,193 @@
+import { Breadcrumb, BreadcrumbItem, Empty, EmptyDescription, EmptyImage, EmptyTitle, Label, Radio, Slider, Input } from "keep-react";
 import { useGetAllServicesQuery } from "../redux/features/services/servicesApi";
+import { BiCaretRight } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
+import { ChangeEvent, useState } from "react";
+import { TService } from "../types/service.types";
+import ServiceCardDetails from "../components/ui/servicePage/serviceCardDetails";
+
+
+const priceRanges = [
+  { label: "0-50", priceRange: [0, 50] },
+  { label: "50-100", priceRange: [50, 100] },
+  { label: "100-150", priceRange: [100, 150] },
+  { label: "150-200", priceRange: [150, 200] },
+  { label: "200-250", priceRange: [200, 250] },
+];
+
 
 const ServicePage = () => {
-  const { data } = useGetAllServicesQuery(undefined);
-  console.log(data);
+  const navigate = useNavigate();
+  const [sort, setSort] = useState("");
+  const [search, setSearch] = useState("");
+  const [priceRange, setPriceRange] = useState<number[] | []>([0, 250]);
+  const { data: services, isLoading: isServiceLoading } = useGetAllServicesQuery([
+    { name: "name", value: search },
+    { name: "priceRange", value: priceRange },
+    { name: "sort", value: sort },
+  ]);
+
+  const onPriceValueChange = (value: number[]) => {
+    setTimeout(() => {
+      setPriceRange(value);
+    }, 300);
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const value = e.target.id;
+    setTimeout(() => {
+      if (name === "search") setSearch(e.target.value);
+    }, 300);
+
+    if (name === "sort") setSort(value);
+
+    if (name === "priceRange") setPriceRange(value.split(",").map((item) => Number(item)));
+  };
 
   return (
-    <div>
-      <h1>Service Page</h1>
-    </div>
+    <>
+      <section className="bg-slate-100">
+        <div className="container flex flex-col items-center">
+          <h2 className="text-4xl font-bold">All Services</h2>
+          <Breadcrumb>
+            <BreadcrumbItem onClick={() => navigate("/")}>Home</BreadcrumbItem>
+            <BreadcrumbItem className="cursor-auto hover:text-black">
+              <BiCaretRight size={18} color="#455468" />
+              Services
+            </BreadcrumbItem>
+          </Breadcrumb>
+        </div>
+      </section>
+      <section className="bg-slate-50">
+        <div className="container">
+          <div className="grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            <div>
+              <div className=" border border-slate-200">
+                <div className="bg-white p-5 pb-0">
+                  <h4 className="text-lg font-semibold border-b border-b-slate-200 pb-2 mb-4">
+                    Search Service
+                  </h4>
+                  <Input
+                    name="search"
+                    placeholder="Write service name..."
+                    onChange={(e) => handleChange(e)}
+                    className="focus-visible:!ring-0 focus-visible:!ring-offset-0 focus-visible:border-cws-yellow"
+                  />
+                </div>
+                <div className="bg-white p-5 pb-0">
+                  <h4 className="text-lg font-semibold border-b border-b-slate-200 pb-2 mb-4">
+                    Price Range
+                  </h4>
+                  <Slider
+                    onValueChange={onPriceValueChange}
+                    min={0}
+                    max={1000}
+                    defaultValue={[0, 1000]}
+                  />
+                  <div className="flex items-center justify-between mt-2">
+                    <span className="text-slate-700">{priceRange[0]}</span>
+                    <span className="text-slate-700">{priceRange[1]}</span>
+                  </div>
+                  <div className="flex flex-col gap-2 mt-2">
+                    {priceRanges?.map((range) => (
+                      <fieldset
+                        key={range.label}
+                        className="flex items-center gap-2"
+                      >
+                        <Radio
+                          onChange={(e) => handleChange(e)}
+                          id={String(range.priceRange)}
+                          name="priceRange"
+                        />
+                        <Label htmlFor={String(range.priceRange)}>
+                          {range.label}
+                        </Label>
+                      </fieldset>
+                    ))}
+                  </div>
+                </div>
+                <div className="bg-white p-5">
+                  <h4 className="text-lg font-semibold border-b border-b-slate-200 pb-2 mb-4">
+                    Sort by
+                  </h4>
+                  <div className="flex flex-col gap-2">
+                    <fieldset className="flex items-center gap-2">
+                      <Radio
+                        defaultChecked={sort === "price"}
+                        onChange={(e) => handleChange(e)}
+                        id="price"
+                        name="sort"
+                      />
+                      <Label htmlFor="price">Price</Label>
+                    </fieldset>
+                    <fieldset className="flex items-center gap-2">
+                      <Radio
+                        defaultChecked={sort === "duration"}
+                        onChange={(e) => handleChange(e)}
+                        id="duration"
+                        name="sort"
+                      />
+                      <Label htmlFor="duration">Duration</Label>
+                    </fieldset>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {services?.data?.length < 1 && !isServiceLoading ? (
+              <div className="col-span-3">
+                <Empty>
+                  <EmptyImage>
+                    <img
+                      src="https://staticmania.cdn.prismic.io/staticmania/a8befbc0-90ae-4835-bf37-8cd1096f450f_Property+1%3DSearch_+Property+2%3DSm.svg"
+                      height={234}
+                      width={350}
+                      alt="404"
+                    />
+                  </EmptyImage>
+                  <EmptyTitle className="mb-[14px] mt-5">
+                    Sorry, No Services Available!
+                  </EmptyTitle>
+                  <EmptyDescription className="mb-8">
+                    We couldn't find any services matching your criteria. Please
+                    try adjusting your filters or check back later for updates.
+                    We're constantly adding new services, so be sure to visit us
+                    again soon!
+                  </EmptyDescription>
+                </Empty>
+              </div>
+            ) : (
+              <div className="md:col-span-2 gap-5 lg:col-span-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                {isServiceLoading
+                  ? Array.from({ length: 6 })?.map((_, id) => (
+                    <div
+                      key={id}
+                      className="bg-white space-y-2 w-full h-full animate-pulse p-5 border border-slate-200"
+                    >
+                      <div className="w-full h-60 sm:h-40 bg-gray-100"></div>
+                      <div className="flex items-center gap-2 w-1/2">
+                        <div className="w-full h-5 bg-gray-100"></div>
+                        <div className="w-full h-5 bg-gray-100"></div>
+                      </div>
+                      <div className="w-full h-7 bg-gray-100"></div>
+                      <div className="space-y-1">
+                        <div className="w-full h-3 bg-gray-100"></div>
+                        <div className="w-full h-3 bg-gray-100"></div>
+                        <div className="w-full h-3 bg-gray-100"></div>
+                      </div>
+                    </div>
+                  ))
+                  : services?.data?.map((service: TService) => (
+                    <div key={service._id}>
+                      <ServiceCardDetails service={service} />
+                    </div>
+                  ))}
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
