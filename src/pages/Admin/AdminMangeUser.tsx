47366a2f67cb847b773770a51a1a0884aa684f9d
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-10-03 02:23:30 +0600

    all dashbosh board not done

--- src/pages/Admin/AdminMangeUser.tsx
+++ src/pages/Admin/AdminMangeUser.tsx
@@ -17,21 +17,16 @@ import { ListOrderedIcon, SearchIcon } from "lucide-react";
 import { useState } from "react";
 import { Separator } from "@/components/ui/separator";
 import ChangeUserRole from "@/components/ChangeRole/ChangeUserRole";
-import {
-  Pagination,
-  PaginationContent,
-  PaginationItem,
-  PaginationLink,
-} from "@/components/ui/pagination";
+
 import { formatDistanceToNow } from "date-fns";
 
 const AdminMangeUser = () => {
-  const [page, setCurrentPage] = useState(1);
+  const [page, setCurrentPage] = useState(2);
   const [limit, setLimit] = useState(10);
   const [searchTerm, setSearchTerm] = useState("");
-  const { data } = useGetAllUsersQuery({ searchTerm, page, limit });
-  const { user } = useAppSelector((state) => state.auth);
-  console.log(data);
+  const { data } = useGetAllUsersQuery(undefined);
+  const { user } = useAppSelector((state: any) => state.auth);
+  console.log(user);
   return (
     <div className="px-4 py-8 sm:px-6">
       <div className="mb-6">
@@ -41,53 +36,7 @@ const AdminMangeUser = () => {
         </p>
       </div>
 
-      <div className="w-full flex items-center justify-between my-6">
-        <form
-          className="flex w-[350px]"
-          onSubmit={(e) => {
-            e.preventDefault();
-            const form = e.target as HTMLFormElement;
-            setSearchTerm(form.search.value);
-          }}
-        >
-          <div className="relative w-full">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <SearchIcon className="h-5 text-muted-foreground w-auto" />
-            </div>
-            <Input
-              type="search"
-              name="search"
-              onBlur={(e) => setSearchTerm(e.target.value)}
-              placeholder="email or last name or first.."
-              className="block w-full p-4 pl-10 text-sm text-foreground bg-background border border-input rounded-md shadow-sm focus:ring-primary focus:border-primary"
-            />
-          </div>
-          <Button type="submit" variant="secondary" className="ml-[10px]">
-            Search
-          </Button>
-        </form>
-        <div className="center gap-[20px]">
-          <Select onValueChange={(e) => setLimit(Number(e))}>
-            <SelectTrigger className="">
-              <ListOrderedIcon className="h-4 w-4" />
-              <SelectValue placeholder="Limit per page" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Content limit</SelectLabel>
-
-                <SelectItem value="10">Limit: 10</SelectItem>
-                <SelectItem value="20">Limit: 20</SelectItem>
-                <SelectItem value="30">Limit: 30</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-      </div>
-
-      {data?.data && data.data.length < 1 ? "<NotFound />" : ""}
-
-      <div className="gap-4 gridUser_responsive">
+      <div className="gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         {searchTerm || page > 1 ? (
           ""
         ) : (
@@ -131,7 +80,7 @@ const AdminMangeUser = () => {
           </Card>
         )}
 
-        {data?.data?.map((pay_user) => (
+        {data?.data?.map((pay_user: any) => (
           <Card key={pay_user._id}>
             <div className="flex items-center gap-4 p-4">
               <Avatar className="h-12 w-12">
@@ -149,6 +98,9 @@ const AdminMangeUser = () => {
                     addSuffix: false,
                   })}
                 </div>
+                <div className="text-sm text-muted-foreground">
+                  Role: {pay_user?.role.toUpperCase()}
+                </div>
               </div>
             </div>
             <Separator />
@@ -160,30 +112,6 @@ const AdminMangeUser = () => {
             </CardContent>
           </Card>
         ))}
-      </div>
-
-      <div className="w-full px-6 flex items-center justify-start gap-[10px] mt-6">
-        <p>Page:</p>
-        <Pagination className="w-fit mx-0">
-          <PaginationContent>
-            {Array.from({
-              length: Math.ceil((data?.totalDoc || 0) / limit),
-            }).map((_, i) => (
-              <PaginationItem key={i + "page"}>
-                <PaginationLink
-                  onClick={() => setCurrentPage(i + 1)}
-                  className={`${
-                    page === i + 1
-                      ? "bg-primary text-muted hover:bg-primary"
-                      : "text-primary"
-                  } border-[1px] border-primary`}
-                >
-                  {i + 1}
-                </PaginationLink>
-              </PaginationItem>
-            ))}
-          </PaginationContent>
-        </Pagination>
       </div>
     </div>
   );
